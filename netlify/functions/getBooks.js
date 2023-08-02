ff3ddf5e1a0c0b0a2fc5c6b0e89c5768dc4ea41a
Half_Barba <barbara.sandrolini@gmail.com>
2023-08-02 16:06:27 +0200

    env var updated

--- netlify/functions/getBooks.js
+++ netlify/functions/getBooks.js
@@ -2,10 +2,9 @@ import axios from 'axios'
 //TODO: import.meta.env??
 export const handler = async (event) => {
     try {
-      console.log(import.meta)
       const { topic, index } = event.queryStringParameters;
       const realIndex = (index - 1) * 10;
-      const apiKey = process.env.VITE_GOOGLEBOOKS_KEY;
+      const apiKey = import.meta.env.env.VITE_GOOGLEBOOKS_KEY;
       let response = await axios.get(
         `https://www.googleapis.com/books/v1/volumes?q=${topic}&startIndex=${realIndex}&maxResults=10&projection=lite&key=${apiKey}`,
         {
